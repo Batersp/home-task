bcfafc72dc532618af3889e7482c1be9935005da
pavel_ishmukov <p-pup1@mail.ru>
2026-02-14 19:51:06 +0300

    updated testing router

--- src/testing/routers/handlers/delete-allData.handler.ts
+++ src/testing/routers/handlers/delete-allData.handler.ts
@@ -1,9 +1,8 @@
 import {Request, Response} from 'express'
-import {db} from "../../../db/in-memory.db";
 import {HttpStatus} from "../../../core/types/http-statuses";
+import {blogCollection, postCollection} from "../../../db/mongo.db";
 
-export function deleteAllData(req: Request, res: Response) {
-    db.blogs = []
-    db.posts = []
+export async function deleteAllData(req: Request, res: Response) {
+    await Promise.all([blogCollection.deleteMany(), postCollection.deleteMany()])
     res.sendStatus(HttpStatus.NoContent)
 }
